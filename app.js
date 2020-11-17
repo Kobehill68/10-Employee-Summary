09a63b1e8f66efda91ada1bf078014f445a935c9
KobeHill68 <kobehill68@gmail.com>
2020-11-17 14:51:30 -0600

    added my questions for each employee

--- app.js
+++ app.js
@@ -11,3 +11,87 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
+const managerQuestions = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "What is the manager's name?"
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's ID number?"
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the manager's email address?"
+    },
+    {
+        type: "input",
+        name: "managerOffice",
+        message: "What is the manager's Office Number?"
+    },
+
+];
+
+const promptquestions = [
+    {
+        type: "list",
+        name: "nextQuestion",
+        message:"Would you like to add more employees to the page?",
+        choices: [
+            "Add an Engineer",
+            "Add an Intern",
+            "Don't add any more employees"
+        ]
+    }
+
+];
+
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "What is the engineer's name?"
+    },
+    {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineer's ID number?"
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's email address?"
+    },
+    {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is the engineer's Github username?"
+    },
+
+];
+
+const internQuestions = [
+    {
+        type: "input",
+        name: "internName",
+        message: "What is the intern's name?"
+    },
+    {
+        type: "input",
+        name: "internId",
+        message: "What is the intern's ID number?"
+    },
+    {
+        type: "input",
+        name: "internEmail",
+        message: "What is the intern's email address?"
+    },
+    {
+        type: "input",
+        name: "internSchool",
+        message: "Where does the intern go to school?"
+    },
+];
